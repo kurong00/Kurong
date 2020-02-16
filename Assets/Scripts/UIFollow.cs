a6b286ee468181610f6772c427feabe6f2efb438
kurong00 <kurong00@gmail.com>
2020-02-16 18:26:20 +0800

    add UIFollow.cs

---
+++ Assets/Scripts/UIFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFollow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject worldPos;
+    [SerializeField]
+    RectTransform rectTrans;
+    public Vector2 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(worldPos.transform.position);
+        rectTrans.position = screenPos + offset;
+    }
+}
