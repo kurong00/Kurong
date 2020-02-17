fb681a6d97249b87e14feddcfaed95568d9f98b5
kurong00 <kurong00@gmail.com>
2020-02-17 21:32:30 +0800

    add head

---
+++ Assets/Scripts/UIToggle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIToggle : MonoBehaviour
+{
+    Toggle toggle;
+    public GameObject player, shadow,shadowCam;
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener(delegate { ToggleValueChanged(toggle); });
+    }
+
+    void ToggleValueChanged(Toggle toggle)
+    {
+        if (toggle.isOn)
+        {
+            player.GetComponent<CharacterController2D>().CanMove = true;
+            shadow.GetComponent<CharacterController2D>().CanMove = false;
+            shadowCam.SetActive(false);
+        }
+        else
+        {
+            player.GetComponent<CharacterController2D>().CanMove = false;
+            shadow.GetComponent<CharacterController2D>().CanMove = true;
+            shadowCam.SetActive(true);
+        }
+    }
+}
