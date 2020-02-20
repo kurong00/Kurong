b793403848abc1e2c0ddd5c612a357ff2d201891
kurong00 <kurong00@gmail.com>
2020-02-20 22:09:14 +0800

    add UIButton

---
+++ Assets/Scripts/UIButtonImage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButtonImage : MonoBehaviour
+{
+    public float holdTime = 1.5f;
+
+    float timer=0;
+
+    public Image image;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (GetComponent<UIButtonLongPress>().isHold)
+        {
+            timer += Time.deltaTime;
+            image.fillAmount = Mathf.Lerp(0, 1, timer / holdTime);
+
+            if (timer >= holdTime)
+            {
+                timer = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIButtonLongPress.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UIButtonLongPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private float holdTime = 1.5f;
+    public bool isHold;
+
+    public UnityEvent onLongPress = new UnityEvent();
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isHold = true;
+        Invoke("OnLongPress", holdTime);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isHold = false;
+        CancelInvoke("OnLongPress");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHold = false;
+        CancelInvoke("OnLongPress");
+    }
+
+    private void OnLongPress()
+    {
+        isHold = true;
+        onLongPress.Invoke();
+    }
+}
