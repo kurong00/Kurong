9dc5362a6cea3a6d56204167d2843d84cf88a5cc
kurong00 <kurong00@gmail.com>
2020-02-21 17:03:54 +0800

    add Scripts

---
+++ Assets/Scripts/UIChangeLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIChangeLight : MonoBehaviour
+{
+    float angel = 0f;
+    public GameObject shadowlight;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Shadow"))
+        {
+            shadowlight.SetActive(true);
+        }
+    }
+
+    public void ChangeAngel()
+    {
+        angel += 90;
+        gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angel));
+    }
+}
